4ce93793b246df7f81f3087da000a5e55b5db456
Leo Magsino <leo.magsino819@gmail.com>
2025-12-22 14:48:26 +0800

    Create project folder structure (lib, types, services)

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+// Shared TypeScript types for ApplyAI
+
+export interface Resume {
+  id: string;
+}
+
+export interface QAEntry {
+  id: string;
+}
+
+export interface JobApplication {
+  id: string;
+}
